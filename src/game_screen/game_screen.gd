59d22a42031dcb65abe1888bb15a38e3aac4199d
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2022-06-11 17:19:30 +0300

    Meeting expiration.

--- src/game_screen/game_screen.gd
+++ src/game_screen/game_screen.gd
@@ -1,11 +1,12 @@
 extends Control
 
 const MEETING_REQUEST_SCENE = preload("res://game_screen/meeting_request.tscn")
-const DAY_COUNT = 5
-const SLOT_COUNT = 18
+const DAY_COUNT: int = 5
+const SLOT_COUNT: int = 18
 const TOTAL_SLOT_COUNT = DAY_COUNT * SLOT_COUNT
 
-export(float) var time_speed = 1.0 / 90.0 / 10  # % per second
+export(float) var time_speed = 1.0 / 90.0 / 2  # % per second
+export(float) var tick_interval = 1.0 # seconds
 
 onready var _gui = $"Gui"
 onready var _cursor: Control = $"Cursor"
@@ -20,17 +21,22 @@ var _is_dragging = false
 var _current_request: MeetingRequest = null
 var _pending_requests: Array = []
 var _slots: Array = []
+var _time_since_tick = 0.0
 
 func _ready() -> void:
 	_slots.resize(TOTAL_SLOT_COUNT)
-	create_request()
-	create_request()
 	create_request()
 
 
 func _process(delta):
 	_time += delta * time_speed
 	_time_shroud.progress = _time
+	
+	_time_since_tick += delta
+	if _time_since_tick >= tick_interval:
+		_time_since_tick = 0.0
+		if randi() % 5 >= _pending_requests.size():
+			create_request()
 
 	if Input.is_action_just_pressed("system_pause"):
 		_gui.pause()
@@ -40,46 +46,50 @@ func _process(delta):
 
 
 func create_request() -> void:
-	var meeting = Meeting.new()
-	meeting.title = "Meet and greet"
-	meeting.duration = 3
-	meeting.special = Meeting.Special.LEISURE
-	
+	var meeting
+	# TODO: adjust difficulty as weeks progress
+	# TODO: pre-generate meetings based on total time
+	match randi() % 10:
+		0:
+			meeting = MeetingGenerator.generate_hard()
+		1,2:
+			meeting = MeetingGenerator.generate_medium()
+		_:
+			meeting = MeetingGenerator.generate_easy()
 	var scene = MEETING_REQUEST_SCENE.instance()
+	scene.is_expiring = true
 	scene.meeting = meeting
 	scene.rect_position.x += _meeting_queue.rect_size.x
 	scene.rect_position.x += scene.rect_size.x * _pending_requests.size()
 	scene.target_position.x = scene.rect_size.x * _pending_requests.size()
-	_meeting_queue.add_child(scene)
-	_pending_requests.append(scene)
 	_setup_request(scene)
 
 
 func place_request(request: MeetingRequest, slot: Vector2) -> void:
-	if request.slot.x > 0:
-		var old_index = request.slot.x * SLOT_COUNT + request.slot.y
+	if request.slot.x >= 0:
+		var old_index = _to_slot_index(request.slot)
 		for i in range(request.meeting.duration):
 			_slots[old_index + i] = null
 
-	var index = slot.x * SLOT_COUNT + slot.y
+	var index = _to_slot_index(slot)
 	for i in range(request.meeting.duration):
 		_slots[index + i] = request
 	request.slot = slot
 
-	_pending_requests.remove(_pending_requests.find(request))
-	for i in range(_pending_requests.size()):
-		var req = _pending_requests[i]
-		req.target_position.x = req.rect_size.x * i
+	_remove_from_pending(request)
 
 	request.is_static = true
 	request.is_resizing = true
+	request.is_expiring = false
+	request.is_expiration_visible = false
 	request.get_parent().remove_child(request)
 	_calendar.add_child(request)
 	request.rect_global_position = _cursor.rect_global_position
 
 
 func can_place_request(request: MeetingRequest, slot: Vector2) -> bool:
-	var index = slot.x * SLOT_COUNT + slot.y
+	var index = _to_slot_index(slot)
+	var lane = index / SLOT_COUNT
 
 	if float(index) / TOTAL_SLOT_COUNT <= _time:
 		return false
@@ -87,11 +97,20 @@ func can_place_request(request: MeetingRequest, slot: Vector2) -> bool:
 	for i in range(request.meeting.duration):
 		if _slots[index + i] != null and _slots[index + i] != request:
 			return false
+		if (index + i) / SLOT_COUNT != lane:
+			return false
 
 	return true
 
 
-func _drag_cursor_start(request) -> void:
+func _drag_cursor_start(request: MeetingRequest) -> void:
+	if not request.is_expiring:
+		var pos = request.rect_position
+		var slot = _to_slot_position(pos)
+		var index = _to_slot_index(slot)
+		if float(index) / TOTAL_SLOT_COUNT <= _time:
+			return
+
 	_cursor.visible = true
 	_is_dragging = true
 	_current_request = request
@@ -100,9 +119,13 @@ func _drag_cursor_start(request) -> void:
 
 
 func _drag_cursor_end(request) -> void:
-	var slot_pos = _to_slot_position(_cursor.rect_position)
-	if can_place_request(request, slot_pos):
-		place_request(request, slot_pos)
+	var cursor_pos = _cursor.rect_position
+	var cal_rect = Rect2(_calendar.rect_position, _calendar.rect_size)
+	
+	if cal_rect.has_point(cursor_pos):
+		var slot_pos = _to_slot_position(_cursor.rect_position)
+		if can_place_request(request, slot_pos):
+			place_request(request, slot_pos)
 
 	_current_request.is_selected = false
 	_cursor.visible = false
@@ -111,7 +134,10 @@ func _drag_cursor_end(request) -> void:
 
 
 func _setup_request(request: MeetingRequest):
+	_meeting_queue.add_child(request)
+	_pending_requests.append(request)
 	request.connect("gui_input", self, "_on_meeting_request_gui_input", [request])
+	request.connect("expired", self, "_on_meeting_request_expired")
 
 
 func _snap(pos: Vector2) -> Vector2:
@@ -126,13 +152,33 @@ func _snap(pos: Vector2) -> Vector2:
 
 func _to_slot_position(pos: Vector2) -> Vector2:
 	pos -= _calendar.rect_position
-	pos.y = floor(pos.y / 16)
-	pos.x = floor(pos.x / 96)
+	pos.y = int(pos.y / 16)
+	pos.x = int(pos.x / 96)
 	return pos
+
+
+func _to_slot_index(slot_pos: Vector2) -> int:
+	return int(slot_pos.x * SLOT_COUNT) + int(slot_pos.y)
+
+
+func _remove_from_pending(request: MeetingRequest) -> void:
+	var index = _pending_requests.find(request)
+	if index < 0:
+		return
+	_pending_requests.remove(index)
+	for i in range(_pending_requests.size()):
+		var req = _pending_requests[i]
+		req.target_position.x = req.rect_size.x * i
 
 
 func _on_meeting_request_gui_input(event: InputEvent, request: MeetingRequest) -> void:
 	if event.is_action_pressed("action"):
 		_drag_cursor_start(request)
-	if event.is_action_released("action"):
+	elif _is_dragging and event.is_action_released("action"):
 		_drag_cursor_end(request)
+
+
+func _on_meeting_request_expired(request: MeetingRequest) -> void:
+	_remove_from_pending(request)
+	request.queue_free()
+
--- src/game_screen/meeting_request.gd
+++ src/game_screen/meeting_request.gd
@@ -1,11 +1,16 @@
 class_name MeetingRequest
 extends Control
 
+signal expired(sender)
+
 export(Resource) var meeting setget set_meeting
 export(bool) var is_resizing = false setget set_is_resizing
 export(float) var scroll_speed = 4
 export(bool) var is_static = false
+export(bool) var is_expiring = false setget set_is_expiring
+export(bool) var is_expiration_visible = true setget set_is_progress_visible
 
+var current_expiration = 0.0
 var target_position = Vector2.ZERO
 var is_selected = false setget set_is_selected
 var slot: Vector2 = Vector2(-1, -1)
@@ -14,12 +19,26 @@ onready var _background: Control = $"Background"
 onready var _title_label: Label = $"MarginContainer/VBoxContainer/TitleLabel"
 onready var _duration_label: Label = $"MarginContainer/VBoxContainer/HBoxContainer/DurationLabel"
 onready var _icon: TextureRect = $"MarginContainer/VBoxContainer/HBoxContainer/Icon"
+onready var _expiration_progress: TextureProgress = $"ExpirationProgress"
 
 func _ready() -> void:
 	set_meeting(meeting)
 
 
 func _process(delta: float) -> void:
+	if is_expiring and current_expiration <= meeting.expiration_time:
+		current_expiration += delta
+		_expiration_progress.value = current_expiration
+		
+		var shake_strength = clamp(current_expiration / meeting.expiration_time - 0.33, 0.0, 1.0)
+		shake_strength = ceil(shake_strength * 4.0)
+		
+		_expiration_progress.material.set_shader_param("shake_strength", shake_strength)
+		_background.material.set_shader_param("shake_strength", shake_strength)
+	elif is_expiring:
+		emit_signal("expired", self)
+		set_is_expiring(false)
+
 	if rect_position != target_position and not is_static:
 		if (target_position - rect_position).length_squared() <= 2:
 			rect_position = target_position
@@ -32,6 +51,7 @@ func _process(delta: float) -> void:
 func set_is_resizing(value: bool) -> void:
 	is_resizing = value
 	if meeting != null and is_inside_tree():
+		_duration_label.get_parent().visible = meeting.duration > 1
 		rect_size.y = meeting.duration * 16
 		rect_size.x = 96 # TODO: magic numbers
 
@@ -39,16 +59,32 @@ func set_is_resizing(value: bool) -> void:
 func set_meeting(value: Meeting):
 	meeting = value
 	if meeting != null and is_inside_tree():
+		_expiration_progress.value = 0.0
+		_expiration_progress.max_value = meeting.expiration_time
 		_title_label.text = meeting.get_short_title()
 		_duration_label.text = meeting.get_duration_string()
 		_icon.texture = meeting.get_icon()
 		_background.material.set_shader_param("tint", meeting.tint)
 		if is_resizing:
+			_duration_label.get_parent().visible = meeting.duration > 1
 			rect_size.y = meeting.duration * 16
 
 
 func set_is_selected(value: bool) -> void:
 	modulate.a = 0.4 if value else 1
+
+
+func set_is_expiring(value: bool) -> void:
+	is_expiring = value
+	if is_inside_tree():
+		_expiration_progress.material.set_shader_param("shake_strength", 0.0)
+		_background.material.set_shader_param("shake_strength", 0.0)
+
+
+func set_is_progress_visible(value: bool) -> void:
+	is_expiration_visible = value
+	if is_inside_tree():
+		_expiration_progress.visible = value
 
 
 func _on_MeetingRequest_gui_input(event: InputEvent) -> void:
